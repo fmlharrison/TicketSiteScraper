f10c416328974509cdd4d66dfea88f6db40fd401
Felix Harrison <fmlharrison@gmail.com>
2018-02-18 15:33:07 +0000

    Add method to get the data structure I want from the site.

--- app/scraper.rb
+++ app/scraper.rb
@@ -1,20 +1,50 @@
 require 'nokogiri'
+require 'mechanize'
 require 'httparty'
 
 class Scraper
   BASE_URL = 'http://www.wegottickets.com/searchresults/all'.freeze
 
-  attr_reader :events_page, :events
+  attr_reader :events_list_page
 
-  def initialize(events = [])
-    @events = events
+  def scrape_events
+    web_page_details
+    events = all_events(events_list_page).map do |event|
+      event_info(event)
+    end
+    events
   end
 
-  def event_links(event_page)
-    parse_page = Nokogiri::HTML(event_page)
-    parse_page.css('a.event_link').each do |link|
-      event_link = link.attributes['href'].value
-      events.push(event_link)
-    end
+  def all_events(tickets_page)
+    tickets_page.search('div.content.block-group.chatterbox-margin')
+  end
+
+  def event_info(event)
+    event_info = {}
+    venue = event.search('div.venue-details h4').first.text.split(':')
+    event_info['artist'] = event.search('a.event_link').text
+    event_info['city'] = venue.first.strip
+    event_info['venue'] = venue.last.strip
+    event_info['date'] = DateTime.parse(event.search('div.venue-details h4')[1].text.to_s)
+    event_info['price'] = event_pricing(event.search('div.block.diptych.text-right'))
+
+    event_info
+  end
+
+  def event_pricing(pricing_element)
+    event_pricing = {}
+    event_pricing['price'] = pricing_element.search('div.searchResultsPrice strong').text
+    event_pricing['ticket_count'] = pricing_element.search('div.buy-stock div').text
+    event_pricing['concession'] = pricing_element.search('span.concession').text
+    event_pricing['warning'] = pricing_element.search('span.warning').text
+
+    event_pricing
+  end
+
+  private
+
+  def web_page_details
+    agent = Mechanize.new
+    @events_list_page = agent.get(BASE_URL)
   end
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,18 +1,53 @@
-require 'spec_helper'
-require 'httparty'
+require 'mechanize'
 require_relative '../app/scraper'
 
 RSpec.describe Scraper do
-  include HTTParty
-
   let(:subject) { described_class.new }
 
-  describe '#event_links' do
-    it 'should return a list of event page links' do
+  before(:all) do
+    VCR.use_cassette('wegottickets/allevents') do
+      events_page = Mechanize.new.get(Scraper::BASE_URL)
+      @events = events_page.search('div.content.block-group.chatterbox-margin')
+    end
+  end
+
+  describe '#all_events' do
+    it 'return a list of 10 events that are on the page' do
+      expect(subject.all_events(@events).length).to eq(10)
+    end
+  end
+
+  describe '#event_info' do
+    it 'should return the info of an event' do
+      event_info = subject.event_info(@events.first)
+      expect(event_info['artist']).to eq('ABSOLUTE & ALMOST BEGINNERS COMEDY COURSE :: STARTS ON SUNDAY 18 FEBRUARY AT NOON (#13A)')
+      expect(event_info['city']).to eq('LONDON')
+      expect(event_info['venue']).to eq("The Albany's Comedy Cellar")
+      expect(event_info['date'].to_s).to eq DateTime.parse('Sun 18th Feb, 2018, 12:00pm').to_s
+      expect(event_info['price']).to include('price' => '£582.00')
+    end
+  end
+
+  describe '#event_pricing' do
+    it 'should return the pricing info of an event' do
+      expected_pricing = {
+        'price' => '£6.60',
+        'ticket_count' => '10 tickets available',
+        'concession' => 'Unwaged - You may be asked to provide eligibility proof at the venue',
+        'warning' => ''
+      }
+
+      event_pricing = subject.event_pricing(@events.last)
+      expect(event_pricing).to eq(expected_pricing)
+    end
+  end
+
+  describe '#scrape_events' do
+    it 'should make a array of all the event details on the page' do
       VCR.use_cassette('wegottickets/allevents') do
-        page = HTTParty.get(Scraper::BASE_URL)
-        subject.event_links(page)
-        expect(subject.events.length).to eq(10)
+        events = subject.scrape_events
+        expect(events).to be_an_instance_of(Array)
+        expect(events.length).to eq(10)
       end
     end
   end
